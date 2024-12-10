9347b57bb623df53ea6271ab028fd7a779d966eb
Josh <joshclarke453@gmail.com>
2024-12-10 18:27:24 -0330

    Day 3 Pt 1

--- Day3/main.rs
+++ Day3/main.rs
@@ -1,3 +1,20 @@
+use std::fs;
+
+use regex::Regex;
+
 fn main() {
-    println!("Wow")
+    let input_content = fs::read_to_string("./input.txt").expect("Should have been able to read the file.");
+    let re = Regex::new(r"mul\([\d]+,[\d]+\)").unwrap();
+    let matches: Vec<&str> = re.find_iter(input_content.as_str()).map(|m| m.as_str()).collect();
+    println!("Fuck");
+    println!("{:?}", matches);
+    let mut sum = 0;
+    for mut m in matches {
+        m = &m[4..(m.len()-1)];  
+        let nums_string: Vec<&str> = m.split(',').collect();
+        let num1: i32 = nums_string[0].parse().unwrap();
+        let num2: i32 = nums_string[1].parse().unwrap();
+        sum += num1 * num2;
+    }
+    println!("{sum}")
 }
