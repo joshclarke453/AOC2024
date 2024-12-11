496a5fd45c2b7adeec12a3e47554b01f338bd3c5
Josh <joshclarke453@gmail.com>
2024-12-10 21:36:56 -0330

    Formatting

--- Day4/main.rs
+++ Day4/main.rs
@@ -1,4 +1,4 @@
-use std::{fs};
+use std::fs;
 
 fn test_index_error(i: i32, j: i32) -> bool {
     let width = 140;
@@ -12,7 +12,6 @@ fn test_index_error(i: i32, j: i32) -> bool {
     return true;
 }
 
-
 fn find_all_words(lines: Vec<Vec<char>>) -> i32 {
     let mut count = 0;
     for i in 0..lines.len() {
@@ -20,83 +19,86 @@ fn find_all_words(lines: Vec<Vec<char>>) -> i32 {
             let int_i = i as i32;
             let int_j = j as i32;
             if test_index_error(int_i, int_j) && lines[i][j] != 'X' {
-                continue
+                continue;
             }
             
-            if test_index_error(int_i-1,int_j-1) && lines[i-1][j-1] == 'M' {
-                if test_index_error(int_i-2,int_j-2) && lines[i-2][j-2] == 'A' {
-                    if test_index_error(int_i-3,int_j-3) && lines[i-3][j-3] == 'S' {
+            if test_index_error(int_i - 1, int_j - 1) && lines[i - 1][j - 1] == 'M' {
+                if test_index_error(int_i - 2, int_j - 2) && lines[i - 2][j - 2] == 'A' {
+                    if test_index_error(int_i - 3, int_j - 3) && lines[i - 3][j - 3] == 'S' {
                         count += 1;
                     }
                 }
             }
             
-            if test_index_error(int_i,int_j-1) && lines[i][j-1] == 'M'{
-                if test_index_error(int_i,int_j-2) && lines[i][j-2] == 'A'{
-                    if test_index_error(int_i,int_j-3) && lines[i][j-3] == 'S'{
+            if test_index_error(int_i, int_j - 1) && lines[i][j - 1] == 'M' {
+                if test_index_error(int_i, int_j - 2) && lines[i][j - 2] == 'A' {
+                    if test_index_error(int_i, int_j - 3) && lines[i][j - 3] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i+1,int_j-1) && lines[i+1][j-1] == 'M'{
-                if test_index_error(int_i+2,int_j-2) && lines[i+2][j-2] == 'A'{
-                    if test_index_error(int_i+3,int_j-3) && lines[i+3][j-3] == 'S'{
+            if test_index_error(int_i + 1, int_j - 1) && lines[i + 1][j - 1] == 'M' {
+                if test_index_error(int_i + 2, int_j - 2) && lines[i + 2][j - 2] == 'A' {
+                    if test_index_error(int_i + 3, int_j - 3) && lines[i + 3][j - 3] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i-1,int_j) && lines[i-1][j] == 'M'{
-                if test_index_error(int_i-2,int_j) && lines[i-2][j] == 'A'{
-                    if test_index_error(int_i-3,int_j) && lines[i-3][j] == 'S'{
+            if test_index_error(int_i - 1, int_j) && lines[i - 1][j] == 'M' {
+                if test_index_error(int_i - 2, int_j) && lines[i - 2][j] == 'A' {
+                    if test_index_error(int_i - 3, int_j) && lines[i - 3][j] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i+1,int_j) && lines[i+1][j] == 'M'{
-                if test_index_error(int_i+2,int_j) && lines[i+2][j] == 'A'{
-                    if test_index_error(int_i+3,int_j) && lines[i+3][j] == 'S'{
+            if test_index_error(int_i + 1, int_j) && lines[i + 1][j] == 'M' {
+                if test_index_error(int_i + 2, int_j) && lines[i + 2][j] == 'A' {
+                    if test_index_error(int_i + 3, int_j) && lines[i + 3][j] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i-1,int_j+1) && lines[i-1][j+1] == 'M'{
-                if test_index_error(int_i-2,int_j+2) && lines[i-2][j+2] == 'A'{
-                    if test_index_error(int_i-3,int_j+3) && lines[i-3][j+3] == 'S'{
+            if test_index_error(int_i - 1, int_j + 1) && lines[i - 1][j + 1] == 'M' {
+                if test_index_error(int_i - 2, int_j + 2) && lines[i - 2][j + 2] == 'A' {
+                    if test_index_error(int_i - 3, int_j + 3) && lines[i - 3][j + 3] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i,int_j+1) && lines[i][j+1] == 'M'{
-                if test_index_error(int_i,int_j+2) && lines[i][j+2] == 'A'{
-                    if test_index_error(int_i,int_j+3) && lines[i][j+3] == 'S'{
+            if test_index_error(int_i, int_j + 1) && lines[i][j + 1] == 'M' {
+                if test_index_error(int_i, int_j + 2) && lines[i][j + 2] == 'A' {
+                    if test_index_error(int_i, int_j + 3) && lines[i][j + 3] == 'S' {
                         count += 1;
                     }
                 }
             }
 
-            if test_index_error(int_i+1,int_j+1) && lines[i+1][j+1] == 'M'{
-                if test_index_error(int_i+2,int_j+2) && lines[i+2][j+2] == 'A'{
-                    if test_index_error(int_i+3,int_j+3) && lines[i+3][j+3] == 'S'{
+            if test_index_error(int_i + 1, int_j + 1) && lines[i + 1][j + 1] == 'M' {
+                if test_index_error(int_i + 2, int_j + 2) && lines[i + 2][j + 2] == 'A' {
+                    if test_index_error(int_i + 3, int_j + 3) && lines[i + 3][j + 3] == 'S' {
                         count += 1;
                     }
                 }
             }
         }
-        
     }
     return count;
 }
 
             
 fn main() {
-    let input_content = fs::read_to_string("./input.txt").expect("Should have been able to read the file.");
+    let input_content =
+        fs::read_to_string("./input.txt").expect("Should have been able to read the file.");
     let strs = input_content.lines().collect::<Vec<&str>>();
-    let chars = strs.iter().map(|s| s.chars().collect()).collect::<Vec<Vec<char>>>();
-    let count = find_all_words(chars);
+    let chars = strs
+        .iter()
+        .map(|s| s.chars().collect())
+        .collect::<Vec<Vec<char>>>();
+    let count = find_all_words(chars.clone());
     println!("{}", count);
 }
